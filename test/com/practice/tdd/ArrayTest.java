1d748b6b960f99463555ca1e0707fa9f4dc69564
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2020-12-04 20:03:11 +0100

    Array

---
+++ test/com/practice/tdd/ArrayTest.java
@@ -0,0 +1,16 @@
+package com.practice.tdd;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ArrayTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
