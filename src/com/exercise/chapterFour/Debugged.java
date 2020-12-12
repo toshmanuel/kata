c9b22b5300da8f5ea6ef1f518f3c8c5892f80d87
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2020-12-12 22:46:32 +0100

    Excercise 4.34(Debug)

---
+++ src/com/exercise/chapterFour/Debugged.java
@@ -0,0 +1,14 @@
+package com.exercise.chapterFour;
+
+public class Debugged {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 6;
+//        System.out.println(++(x + y));
+        System.out.println((x + y)+1);
+    }
+}
+
+//(What’s Wrong with This Code?) What is wrong with the following statement? Provide the
+//        correct statement to add one to the sum of x and y.
+//        System.out.println(++(x + y));
